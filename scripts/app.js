29e5e7340799e21f6af5a608fac59b1d31940808
Your Name <you@example.com>
2021-03-25 13:37:38 +0000

    first commit for weather application

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+const  cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (response) => {
+  
+  // destructor properties
+  const { cityDetails, weather } = response;
+
+  //update details template
+  details.innerHTML = `          
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+    `;
+    //remove the class of d-none if present
+    if(card.classList.contains('d-none')){
+      card.classList.remove('d-none');
+    }
+};
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+  return { cityDetails, weather };
+};
+
+cityForm.addEventListener('submit', e => {
+  //prevent default action
+  e.preventDefault();
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+  // update the ui with new city
+  updateCity(city).then(response => updateUI(response))
+});
