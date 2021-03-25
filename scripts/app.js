6abb2c326b57afe1d7bebed543fb09563c2a5313
Your Name <you@example.com>
2021-03-25 17:58:18 +0000

    fixed icon

--- scripts/app.js
+++ scripts/app.js
@@ -18,10 +18,23 @@ const updateUI = (response) => {
       <span>&deg;C</span>
     </div>
     `;
+
+    //update the night/ day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    let timeSrc = null;
+    if(weather.IsDayTime){
+      timeSrc = 'img/day.svg';
+    } else {
+      timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
     //remove the class of d-none if present
     if(card.classList.contains('d-none')){
       card.classList.remove('d-none');
     }
+   
 };
 
 const updateCity = async (city) => {
